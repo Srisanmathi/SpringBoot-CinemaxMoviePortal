b6a1aabf96639230ea9fe36e9db6bd1839c86592
Srisanmathi <sanmathi3896@gmail.com>
2020-05-04 16:18:45 -0400

    before database integration

--- src/main/java/com/api/MovieController.java
+++ src/main/java/com/api/MovieController.java
@@ -1,9 +1,7 @@
 package com.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,5 +20,20 @@ public class MovieController {
     @RequestMapping("/movies/{id}")
     public Movie  getMovie(@PathVariable String id){
         return movieService.getMovie(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST,value = "/movies")
+    public void addMovie(@RequestBody Movie movie){
+        movieService.addMovie(movie);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT,value = "/movies/{id}")
+    public Movie updateMovie(@RequestBody Movie movie,@PathVariable String id ){
+        return movieService.updateMovie(id, movie);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE,value = "/movies/{id}")
+    public void deleteMovie(@PathVariable String id){
+        movieService.deleteMovie(id);
     }
 }
--- src/main/java/com/api/MovieService.java
+++ src/main/java/com/api/MovieService.java
@@ -8,10 +8,10 @@ import java.util.List;
 
 @Service
 public class MovieService {
-    private List<Movie> movies = Arrays.asList(
+    private List<Movie> movies = new ArrayList<>(Arrays.asList(
             new Movie("1","Avengers","adventure",2019),
             new Movie("2","Wonder Women","adventure",2018)
-            );
+            ));
 
     public List<Movie> getAllMovies(){
         return movies;
@@ -19,5 +19,28 @@ public class MovieService {
 
     public Movie getMovie(String id){
         return movies.stream().filter(m -> m.getId().equals(id)).findFirst().get();
+    }
+
+    public void addMovie(Movie movie){
+        movies.add(movie);
+    }
+
+    public Movie updateMovie(String id, Movie movie){
+        for(int i=0;i<movies.size();i++){
+            if(movies.get(i).getId().equals(id)){
+                movies.set(i,movie);
+                return movies.get(i);
+            }
+        }
+        return null;
+    }
+
+    public Movie deleteMovie(String id){
+        for(int i=0;i<movies.size();i++){
+            if(movies.get(i).getId().equals(id)){
+                movies.remove(i);
+            }
+        }
+        return null;
     }
 }
