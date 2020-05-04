0cc3c38c9b4303761031e9d2a4996a36dba95ebe
Srisanmathi <sanmathi3896@gmail.com>
2020-05-04 14:48:10 -0400

    Added getMovie route

--- src/main/java/com/api/MovieController.java
+++ src/main/java/com/api/MovieController.java
@@ -1,6 +1,7 @@
 package com.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,10 @@ public class MovieController {
     @RequestMapping("/movies")
     public List<Movie> getAllMovies () {
         return movieService.getAllMovies();
+    }
+
+    @RequestMapping("/movies/{id}")
+    public Movie  getMovie(@PathVariable String id){
+        return movieService.getMovie(id);
     }
 }
